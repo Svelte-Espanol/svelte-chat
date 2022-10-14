6f429f7ce0fb43cfd6f129892897176b4ba2e92c
Irwing Naranjo <khalisser@gmail.com>
2022-10-13 22:34:55 -0300

    Init porject

---
+++ frontend/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import geckos from "@geckos.io/client";
+  const channel = geckos({ url: 'http://localhost:4000', port: null });
+  channel.onConnect((err) => console.log(err));
+
+  let menssages = {
+    android: [],
+  };
+
+  channel.on('app_android', (data) => {
+    menssages.android = menssages.android.concat(data);
+  });
+</script>
+
+<main>
+  Hola mundo sockets
+
+  <div>
+    <h1>Android</h1>
+    <ul>
+      {#each menssages.android as menssage}
+        <li>{menssage}</li>
+      {/each}
+    </ul>
+  </div>
+</main>
---
+++ server/server.js
@@ -0,0 +1,59 @@
+// BACK
+
+// import other libraries as CJS
+const express = require('express')
+const http = require('http')
+const app = express()
+const server = http.createServer(app)
+const port = 4000
+
+// let players = {}
+
+
+const main = async () => {
+
+  const { geckos } = await import('@geckos.io/server');
+
+  const io = geckos()
+
+  io.addServer(server)
+  io.onConnection(channel => {
+
+    console.log("ID ", channel.id)
+
+    // players[channel.id] = {
+    //   playerId: channel.id,
+    //   name: randomName,
+    // }
+
+    setInterval(() => {
+      const randomName = Math.random().toString(36).substring(7)
+      channel.emit('app_android', randomName)
+    }, 4000)
+
+
+    // channel.on('message', (data) => {
+    //   const obj = { name: players[channel.id].name, message: data.message }
+    //   channel.room.emit('message-for-all', obj)
+    // })
+
+    // console.log(channel)
+
+    // channel.onDisconnect(() => {
+    //   delete channel.id;
+    //   console.log(`${channel.id} got disconnected`)
+    // })
+  })
+
+  // create a GET route
+  app.get('/', (req, res) => {
+    res.send({ response: 'I am alive' }).status(200)
+  })
+
+  server.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+
+}
+
+main()
