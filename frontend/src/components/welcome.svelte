e03adebe13d7aebcd091d37537fb71f8a3ad591c
vitalspace <myglobalsounds@gmail.com>
2022-10-21 14:35:39 -0500

    welcome component

---
+++ frontend/src/components/welcome.svelte
@@ -0,0 +1,23 @@
+<div
+  class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12"
+>
+  <div
+    class="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10"
+  >
+    <img
+      class="mx-auto my-4"
+      src="https://www.svgrepo.com/show/428665/thai-pattern-architecture-6.svg"
+      alt=""
+    />
+    <h1 class="my-4 text-2xl">Hello there, Welcome.</h1>
+    <div class="my-4">
+      <p>Enter the name by which you will be recognized in the room.</p>
+    </div>
+    <div class="flex">
+      <input class="w-full border-2 py-2 pl-4" type="text" />
+      <button class="bg-green-400 px-5 py-3 text-white hover:bg-green-300"
+        >Go</button
+      >
+    </div>
+  </div>
+</div>
